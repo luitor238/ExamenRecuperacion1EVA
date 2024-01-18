b6b11d0b783c6a4c0face3921cd3421d52104832
luitor238 <luistorgacastro@gmail.com>
2024-01-18 10:28:40 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ExamenRecuperacion1ºEVA"
+include(":app")
+ 
